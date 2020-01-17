6872090651e0fbd2922e0fbb36def484e4881a75
Malena Rey <malerey@male.5d640511d691.sn.mynetname.net>
2020-01-17 19:58:09 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+
+const imagesAsObjects = [
+  {
+    title: "Forest",
+    img: "https://www.w3schools.com/css/img_forest.jpg"
+  },
+  {
+    title: "5 terre",
+    img: "https://www.w3schools.com/css/img_5terre.jpg"
+  },
+  {
+    title: "Northern lights",
+    img: "https://www.w3schools.com/css/img_lights.jpg"
+  },
+  {
+    title: "Mountains",
+    img: "https://www.w3schools.com/css/img_mountains.jpg"
+  }
+]
+
+let acumuladoraCards = "";
+
+for (let i = 0; i < imagesAsObjects.length; i++) {
+  acumuladoraCards += `<div class="card">
+  <img src=${imagesAsObjects[i].img} alt=${imagesAsObjects[i].title}>
+<div class="desc">${imagesAsObjects[i].title}</div>
+</div>`;
+}
+
+const containerHtml = document.getElementById("container");
+
+containerHtml.innerHTML = acumuladoraCards;
+  
+  
+
+
+
+const modal = document.getElementById("myModal");
+
+const botonAbrirModal = document.getElementById("mostrarModal");
+const botonCerrarModal = document.getElementById("cerrarModal")
+
+botonAbrirModal.onclick = () => {
+  modal.classList.remove("nomostrar") 
+}
+
+botonCerrarModal.onclick = () => {
+  modal.classList.add("nomostrar")
+}
